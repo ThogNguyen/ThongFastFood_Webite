3730183c8c4064e23a2c90ab5b1b6c7c8ba82763
ThogNguyen <thoggnguyen2004@gmail.com>
2024-05-30 14:34:39 +0700

    update: load sản phẩm trang chủ(client)

--- ThongFastFood_Client/Controllers/MainPageController.cs
+++ ThongFastFood_Client/Controllers/MainPageController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ThongFastFood_Api.Models;
 
 namespace ThongFastFood_Client.Controllers
 {
 	public class MainPageController : Controller
 	{
-		public IActionResult Index()
+		Uri baseUrl = new Uri("https://localhost:7244/api");
+
+		private readonly HttpClient _httpClient;
+		private readonly IWebHostEnvironment _environment;
+		public MainPageController(HttpClient httpClient, IWebHostEnvironment environment)
 		{
-			return View();
+			_httpClient = httpClient;
+			_httpClient.BaseAddress = baseUrl;
+			_environment = environment;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			List<ProductVM> products = new List<ProductVM>();
+			HttpResponseMessage apiMessage =
+			   await _httpClient.GetAsync(_httpClient.BaseAddress + "/ProductApi/GetProducts");
+
+			if (apiMessage.IsSuccessStatusCode)
+			{
+				string data = await apiMessage.Content.ReadAsStringAsync();
+				products = JsonConvert.DeserializeObject<List<ProductVM>>(data);
+			}
+			return View(products);
 		}
 		public IActionResult About()
 		{
